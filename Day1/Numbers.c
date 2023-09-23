4ba627ddbca83159cef4936f25af5c0ec9a13d5b
Yerukondu Haritha <yerukonduharitha@gmail.com>
2023-09-23 16:36:00 +0530

    Day1 work done

---
+++ Day1/Numbers.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+    int b;
+    printf("%d",b); //if not inialize the value it print garbage value
+    int a=22;
+    printf(" ");
+    printf("%d",a);
+    return 0;
+}
